d83a6235b21de54c8c458d0a82a6bfa635348c47
GFurtado <gfurtado.sw@gmail.com>
2025-04-10 12:46:42 +0100

    done

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
-
+const app = express();
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
 const logger = require("morgan");
@@ -11,28 +11,24 @@ const cookieParser = require("cookie-parser");
 
 // ℹ️ Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
 // unless the request is made from the same domain, by default express wont accept POST requests
-const cors = require("cors");
+const cors = require('cors');
+
+app.use(cors());
+
 
 const FRONTEND_URL = process.env.ORIGIN || "http://localhost:5173";
 
-// Middleware configuration
 module.exports = (app) => {
-  // Because this will be hosted on a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-  // Services like Fly use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
 
-  // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: [FRONTEND_URL, "http://localhost:5178"], // Add the new frontend URL here
       credentials: true,
     })
   );
 
-  // In development environment the app logs
   app.use(logger("dev"));
-
-  // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
