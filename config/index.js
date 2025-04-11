2f433ee1ff4f164ffc95be4ee7a8f742af7c24b0
GFurtado <gfurtado.sw@gmail.com>
2025-04-11 10:08:34 +0100

    letsgoo

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 // :information_source: Needed to accept requests from 'the outside'. CORS stands for cross origin resource sharing
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
-const FRONTEND_URL = process.env.ORIGIN || "https://surfology.netlify.app/"; 
+const FRONTEND_URL = process.env.ORIGIN || "https://surfology.netlify.app"; 
 // Middleware configuration
 module.exports = (app) => {
  
